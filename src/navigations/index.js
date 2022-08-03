90caa0962fa60851d5676684ca3ea08195eb09c8
Syed Muhamamd Murtaza <syed.murtaza@rocketmail.com>
2022-08-03 07:09:08 +0500

    updated logs!

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -9,6 +9,10 @@ import Home from '../components/Home';
 import Details from '../components/Details';
 import Favorites from '../components/Favorites';
 
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
 const Stack = createNativeStackNavigator();
 
 function AppNavigator() {
